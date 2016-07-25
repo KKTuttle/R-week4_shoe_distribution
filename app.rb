ae86df55c13a5d437a39647ef2aad30c59c6265e
Katie Tuttle <katka.karolcikova@gmail.com>
2016-07-25 15:54:44 -0700

    allow user to add brand to a store and store to a brand

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-
 get('/') do
   @stores = Store.all()
   @brands = Brand.all()
@@ -14,10 +13,52 @@ post('/stores') do
   @store = Store.new({:name => name, :id => nil})
   @stores = Store.all()
   if @store.save()
-    erb(:index)
+    redirect to("/")
   else
     erb(:error_store)
   end
+end
+# ADDING BRAND
+post('/brands') do
+  name = params.fetch('brand_name')
+  @brand = Brand.new({:name => name, :id => nil})
+  @brands = Brand.all()
+  if @brand.save()
+    redirect to("/")
+  else
+    erb(:error_brand)
+  end
+end
+
+get('/brand/:id') do
+  @stores = Store.all()
+  @brand = Brand.find(params.fetch('id').to_i())
+  erb(:brand)
+end
+# select from brand page
+post '/brand/:id/store' do
+  @stores = Store.all()
+  brand_id = params['brand_id'].to_i
+  store_id = params['store_id'].to_i
+  store = Store.find(store_id)
+  @brand = Brand.find(params['id'].to_i)
+  if !@brand.stores.include?(store)
+    @brand.stores.push(store)
+  end
+  redirect to("/brand/#{@brand.id()}")
+end
+
+# select from store page
+post '/store/:id/brand' do
+  @brands = Brand.all()
+  store_id = params['store_id'].to_i
+  brand_id = params['brand_id'].to_i
+  brand = Brand.find(brand_id)
+  @store = Store.find(params['id'].to_i)
+  if !@store.brands.include?(brand)
+    @store.brands.push(brand)
+  end
+  redirect to("/store/#{@store.id()}")
 end
 
 get('/stores/:id/edit') do
@@ -42,20 +83,8 @@ end
 
 get('/store/:id') do
   @store = Store.find(params.fetch('id').to_i())
+  @brands = Brand.all()
   erb(:store)
-end
-# ADDING A BRAND TO A STORE
-post('/store/:id') do
-  name = params.fetch('brand_name')
-  store_id = params.fetch('store_id').to_i()
-  @brand = Brand.new({:name => name, :id => nil})
-  @store = Store.find(store_id)
-  @store.brands.create({:name => name})
-  if @brand.save()
-    redirect to("/store/#{@store.id()}")
-  else
-    erb(:error_brand)
-  end
 end
 
 get("/clear") do
